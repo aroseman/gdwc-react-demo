f3e4ebb52e55d888f6bcf55c3a8309a88d3a1b90
Andrew Roseman <amr6163@psu.edu>
2021-04-14 14:57:40 -0400

    Initial Push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import '@gdwc/components/menu';
 function App() {
   return (
     <div className="App">
+      <gdwc-menu branding="Menu Heading" baseUrl="https://decoupled-menus.jsonapi.dev" menuId="main"></gdwc-menu>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
